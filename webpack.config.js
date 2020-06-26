30ebcfbde5d13ab02ae1a063db5270b55eb598b6
Nvelia <nicolas.velia.ji@gmail.com>
2020-06-26 12:12:08 +0200

    web 8

--- webpack.config.js
+++ webpack.config.js
@@ -13,21 +13,21 @@ module.exports = {
   //     host: '0.0.0.0',
   //     port: 8080,
   // },
-  // module: {
-  //     loaders: [
-  //         {
-  //             test: /\.(jpg|jpeg|gif|png|ico)$/,
-  //             exclude: /node_modules/,
-  //             loader: 'file-loader?name=[name].[ext]'
-  //         },
-  //         {
-  //             test: /\.js$/,
-  //             exclude: /node_modules/,
-  //             loader: 'babel-loader',
-  //             query: {
-  //                 presets: ["es2016", "react", "env", "stage-2"]
-  //             }
-  //         }
-  //     ]
-  // }
+  module: {
+    loaders: [
+      {
+        test: /\.(jpg|jpeg|gif|png|ico)$/,
+        exclude: /node_modules/,
+        loader: "file-loader?name=[name].[ext]",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2016", "react", "env", "stage-2"],
+        },
+      },
+    ],
+  },
 };
